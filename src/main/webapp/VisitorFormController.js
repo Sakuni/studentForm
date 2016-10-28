b7f2faebea3f21763291bf3024de6cc093368186
Sakuni <sakmanamendra@gmail.com>
2016-10-28 17:00:55 +0530

    popup added

--- src/main/webapp/VisitorFormController.js
+++ src/main/webapp/VisitorFormController.js
@@ -9,7 +9,7 @@ angular.module('testApp', ['ngImgCrop']).
             {
                 "id": 1,
                 "firstName": "Curly",
-                "lastName": "Howard",
+                "lastName": "Fox",
                 "address": "4524 Fulton Avenue",
                 "city": "Van Nuys",
                 "state": "CA",
@@ -114,5 +114,57 @@ angular.module('testApp', ['ngImgCrop']).
         };
         angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
 
+        //
+        $scope.showModal = false;
+        $scope.buttonClicked = "";
+        $scope.toggleModal = function (btnClicked) {
+            $scope.buttonClicked = btnClicked;
+            $scope.showModal = !$scope.showModal;
+        };
+
     }
-);
+
+
+
+)
+    .directive('modal', function () {
+        return {
+            template: '<div class="modal fade">' +
+            '<div class="modal-dialog">' +
+            '<div class="modal-content">' +
+            '<div class="modal-header">' +
+            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+            '<h4 class="modal-title">{{ buttonClicked }} clicked!!</h4>' +
+            '</div>' +
+            '<div class="modal-body" ng-transclude></div>' +
+            '</div>' +
+            '</div>' +
+            '</div>',
+            restrict: 'E',
+            transclude: true,
+            replace:true,
+            scope:true,
+            link: function postLink(scope, element, attrs) {
+                scope.$watch(attrs.visible, function(value){
+                    if(value == true)
+                        $(element).modal('show');
+                    else
+                        $(element).modal('hide');
+                });
+
+                $(element).on('shown.bs.modal', function(){
+                    scope.$apply(function(){
+                        scope.$parent[attrs.visible] = true;
+                    });
+                });
+
+                $(element).on('hidden.bs.modal', function(){
+                    scope.$apply(function(){
+                        scope.$parent[attrs.visible] = false;
+                    });
+                });
+            }
+        };
+    })
+;
+
